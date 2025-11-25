5351213c61655e8f7840b48ade08a00319b203ef
Kelly Barbosa <kelly.189@hotmail.com>
2025-11-24 23:58:55 -0300

    Adicionando novo tema

--- EstruturaDados/src/main/java/br/edu/ufrb/estruturadados/EstruturaDados.java
+++ EstruturaDados/src/main/java/br/edu/ufrb/estruturadados/EstruturaDados.java
@@ -1,19 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.edu.ufrb.estruturadados;
 
-import javax.swing.SwingUtilities;
-
-/**
- *
- * @author 2401210
- */
 public class EstruturaDados {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println("Não foi possível aplicar o Look and Feel Nimbus.");
+        }
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
             VisualizadorFrame frame = new VisualizadorFrame();
             frame.setVisible(true);
         });
